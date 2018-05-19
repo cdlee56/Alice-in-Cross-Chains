630908658aa7a58453aa15a1b081a86aa631dccf
mibzman <sam@hungerperks.com>
2018-05-19 15:31:50 -0500

    fixed type errors

--- contracts/ChainOfCustody.sol
+++ contracts/ChainOfCustody.sol
@@ -1,17 +1,10 @@
 pragma solidity ^0.4.18;
 contract ChainOfCustody {
 
-    mapping (uint => address) public profToOwner;
-
-    struct Bid {
-        address bidder;
-        uint value;
-    }
-
-    modifier onlyOwnedBy(address _requester, uint _profId) {
-    	require(profToOwner[_profId] == _requester);
-      	_;
-    }
+    // modifier onlyOwnedBy(address _requester, uint _profId) {
+    // 	require(profToOwner[_profId] == _requester);
+    //   	_;
+    // }
 
     function ChainOfCustody() public payable {
     }
@@ -25,8 +18,8 @@ contract ChainOfCustody {
     }
 
     struct Precinct{
-    	uint precinctID;
-        string precinctName; 
+    	uint ID;
+        string Name; 
     }
 
     mapping (address => Actor) actorMap;
@@ -35,13 +28,13 @@ contract ChainOfCustody {
     function NewPrecinct(
     	uint precinctID,
         string precinctName,
-        string sherrifName, 
+        string actorName, 
         string badgeNumber, 
         string title) 
     public {
-        Precinct newPrecinct = Precinct(precinctID, precinctName);
-        precinctMap[precinctID] = newPrecinct; //Map id to precinct
-        NewActor(precinctID, sherrifName, badgeNumber, title, true); //Create first admin
+        // Precinct newPrecinct = 
+        precinctMap[precinctID] = Precinct(precinctID, precinctName); //Map id to precinct
+        NewActor(precinctID, actorName, badgeNumber, title, true); //Create first admin
     }
 
     function NewActor(
@@ -51,7 +44,7 @@ contract ChainOfCustody {
         string title,
         bool isAdmin) 
     public {
-    	Actor newactor = Actor(precinctID, name, badgeNumber, title, isAdmin); //Create new actor instance
-        actorMap[msg.sender] = newactor; //Map address to actor
+    	// Actor storage newactor = 
+        actorMap[msg.sender] = Actor(precinctID, name, badgeNumber, title, isAdmin); //Create new actor instance
     }
 }
